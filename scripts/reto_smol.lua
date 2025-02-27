74e30928432bf28eff7d633a9a9aa377f5000728
Gnarty/Trever <58467534+Gnarty@users.noreply.github.com>
2025-02-26 23:31:10 -0600

    preparacion del reto smol

--- scripts/reto_smol.lua
+++ scripts/reto_smol.lua
@@ -1,34 +1,11 @@
 local challenge = Isaac.GetChallengeIdByName("With love from smol")
 
 
---[[  codigo del antiguo reto con esau
-local player = Isaac.GetPlayer()
-local personaje1 = PlayerType.PLAYER_JACOB_B
-local room = Game():GetRoom()
--- Spawn de los 3 esau
-function EBOI_EVENT:iniciador_smol()
-    if Isaac.GetChallenge() ~= challenge then return end
-    Isaac.Spawn(EntityType.ENTITY_DARK_ESAU, 0, 1, Vector(520,280), Vector(0,0),player)
-    Isaac.Spawn(EntityType.ENTITY_DARK_ESAU, 0, 1, Vector(100,280), Vector(0,0),player)
-    Isaac.Spawn(EntityType.ENTITY_DARK_ESAU, 0, 1, Vector(320,180), Vector(0,0),Isaac.GetPlayer(0))
-    print("funciona, personaje: ", player:GetPlayerType())
-    for i, entity in ipairs(Isaac.GetRoomEntities()) do
-        print(entity.Type, entity.Variant, entity.SubType)
-    end
-    
-    
+
+
+function EBOI_EVENT:cambiador_de_items_smol(player)
+
 end
 
 
-EBOI_EVENT:AddCallback(ModCallbacks.MC_POST_GAME_STARTED,EBOI_EVENT.iniciador_smol)
-
-
--- testeos para ver si era un problema con el personaje lo de los esaus
-function EBOI_EVENT:test()
-    if Isaac.GetChallenge() ~= challenge then return end
-    Player:ChangePlayerType(PlayerType.PLAYER_JACOB)
-end
-
-EBOI_EVENT:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT,EBOI_EVENT.test)
-
-]]--
+EBOI_EVENT:AddCallback(ModCallbacks.MC_POST_NEW_ROOM,EBOI_EVENT.cambiador_de_items_smol)
