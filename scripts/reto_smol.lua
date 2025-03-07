702fed3e03faa4edf508e93c714b235e88a708af
Gnarty/Trever <58467534+Gnarty@users.noreply.github.com>
2025-03-07 01:16:37 -0600

    areglos del reto smol

--- scripts/reto_smol.lua
+++ scripts/reto_smol.lua
@@ -4,7 +4,8 @@ local challenge = Isaac.GetChallengeIdByName("With love from smol")
 
 
 function EBOI_EVENT:cambiador_de_items_smol()
-    items = {
+    local regalo_smol = Isaac.GetItemIdByName("Smol's Present")
+    local items_regalo = {
         { id = 644, weight = 50, name = "Consolation Prize" },
         { id = 118, weight = 1, name = "Brimstone" },
         { id = 182, weight = 1, name = "Sacred Heart" },
@@ -16,27 +17,38 @@ function EBOI_EVENT:cambiador_de_items_smol()
         { id = 652, weight = 6, name = "Cube Baby" },
         { id = 273, weight = 6, name = "Bob's Brain" },
         { id = 15, weight = 6, name = "<3" },
+        { id = regalo_smol, weight = 6, name = "Smol's Present" },
     }
 
     for i, entity in ipairs(Isaac.GetRoomEntities()) do
         pickup = entity:ToPickup()
-        if entity.Type == EntityType.ENTITY_PICKUP and not eevee_uso_el_item and entity.Variant == 100 then
-            
-            print(entity.Type, entity.SubType,entity.Variant, pickup.Wait )
-           
-        elseif entity.Type == EntityType.ENTITY_PICKUP and eevee_uso_el_item and entity.Variant == 100 then
-            --print(entity.Type, entity.SubType, pickup.Wait )
-            pickup.Wait = 0
-            --print(pickup.Timeout)
-            if pickup.Timeout < 5 and pickup.Timeout ~= -1  then
-                sfxManager:Play(SoundEffect.SOUND_ULTRA_GREED_COIN_DESTROY, 1, 4, false, 1, 0)
+        
+        
+        if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == 100 then
+            local esta_en_la_lista = false
+            if entity.Type == EntityType.ENTITY_PICKUP then
+                local esta_en_la_lista = false
+    
+                for _, j in ipairs(items_regalo) do
+                    if j.id == entity.SubType then
+                        esta_en_la_lista = true
+                        break
+                    end
+                end
+                
+                if not esta_en_la_lista then
+                    print(entity.Type, entity.SubType,entity.Variant, pickup.Wait, "item no encontrado")
+                    pickup:Morph(entity.Type, entity.Variant, regalo_smol, KeepPrice, false, true)
+                    print(entity.Type, entity.SubType,entity.Variant, pickup.Wait, "nuevo item smol")
+                end
             end
-            local sprite = entity:GetSprite()
-            sprite.Color = Color(1,1,1,pickup.Timeout/100/2)
-            
         end
+            
+           
+            
+        
     end
 end
 
 
-EBOI_EVENT:AddCallback(ModCallbacks.MC_POST_NEW_ROOM,EBOI_EVENT.cambiador_de_items_smol)
+EBOI_EVENT:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE,EBOI_EVENT.cambiador_de_items_smol)
